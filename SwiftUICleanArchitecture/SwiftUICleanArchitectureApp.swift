07c2acc3e9364a56e945e68cbca1ac55aaeed8bc
Fhikar Zul <Fhikarzul18@gmail.com>
2022-09-01 13:52:30 +0800

    Initial Commit

---
+++ SwiftUICleanArchitecture/SwiftUICleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICleanArchitectureApp.swift
+//  SwiftUICleanArchitecture
+//
+//  Created by Lucy on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
